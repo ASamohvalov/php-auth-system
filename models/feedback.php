37cf2f4a4c61079d85f04df767dba496379e195b
artem samohvalov <artym.samohvaloff@gmail.com>
2025-10-20 20:48:18 +0400

    models init

--- models/feedback.php
+++ models/feedback.php
@@ -3,16 +3,42 @@ session_start();
 
 require_once '../db/sql_executor.php';
 
-
-function get_user_feedbacks() : array
+class Feedback
 {
-  $sql_select = 'select * from feedback where user_id = :user_id';
-  $result_array = perfom_and_get($sql_select, [
-    'user_id' => $_SESSION['user']['id']
-  ]);
-  return $result_array;
+  public function __construct(
+      public string $id,
+      public string $title,
+      public string $message,
+      public string $request_type,
+      public int $rating,
+      public int $user_id) {}
+
+  public function save() : void
+  {
+   $sql_insert = 'insert into feedback (title, message, request_type, rating, user_id)' 
+    . 'values (:title, :message, :request_type, :rating, :user_id)';
+    perfom($sql_insert, [
+      'title' => $this->title,
+      'message' => $this->message,
+      'request_type' => $this->request_type,
+      'rating' => $this->rating,
+      'user_id' => $this->user_id
+    ]); 
+  } 
+
+  public static function get_user_feedbacks(int $user_id) : Feedback 
+  {
+    $sql_select = 'select * from feedback where user_id = :user_id';
+    $result_array = perfom_and_get($sql_select, ['user_id' => $user_id]);
+    return new Feedback(
+      $result_array[0]['id'],
+      $result_array[0]['titile'],
+      $result_array[0]['message'],
+      $result_array[0]['request_type'],
+      $result_array[0]['rating'],
+      $result_array[0]['user_id']
+    );
+  }
 }
 
-function send_feedback() : void
-{
-}
+
---
+++ models/user.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once '../db/sql_executor.php';
+
+class User
+{
+  public function __construct(
+      public int $id,
+      public string $email,
+      public string $password,
+      public string $name,
+      public string $surname) {}
+
+  public function save() : void
+  {
+    $sql_insert = 'insert into users (email, password, name, surname) values (:email, :password, :name, :surname)';
+    perfom($sql_insert, [
+      'email' => $this->email,
+      'password' => password_hash($this->password, PASSWORD_DEFAULT),
+      'name' => $this->name,
+      'surname' => $this->surname
+    ]);
+  }
+
+  public function validate_password(string $password) : bool
+  {
+    return password_verify($password, $this->password);
+  }
+
+  public static function exists_by_email(string $email) : bool
+  {
+    $sql_select = 'select count(*) as cnt from users where email = :email';
+    $result = perfom_and_get($sql_select, ['email' => $email]);
+    return $result['0']['cnt'] == 0;
+  }
+
+  public static function get_by_email(string $email) : User
+  {
+    $sql_select = 'select * from users where email = :email';
+    $result_array = perfom_and_get($sql_select, ['email' => $email]);
+    return new User(
+      $result_array[0]['id'],
+      $result_array[0]['email'],
+      $result_array[0]['password'],
+      $result_array[0]['name'],
+      $result_array[0]['surname']
+    );
+  }
+}
